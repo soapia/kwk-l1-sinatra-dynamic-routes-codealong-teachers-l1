3d172ad80f794f1008f95a734eaf1faeddadcea5
sofia ongele <sofiaongele@gmail.com>
2018-06-12 15:34:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get "/multiply" do
-    
+  get "/multiply/:num1/:num2" do
+    @product = params[:num1] * params[:num2]
   end
   
 end
